e2ca87ebdb4896d8001a125a79a2eb52f8a98eaa
rybakouvlad <rybakou.vladik@gmail.com>
2021-03-06 21:26:00 +0300

    feat: add launguage.js

--- src/client/components/Footer.tsx
+++ src/client/components/Footer.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import lang from '../language'
 export const Footer: React.FC = () => {
   return (
     <footer>
       <h1>FOOOTER</h1>
+      <h1>{lang.country.ru}</h1>
     </footer>
   );
 };
