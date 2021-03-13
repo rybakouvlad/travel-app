9621439931a6e3d39732efd8375a4a9def005a3a
Tatsiana Novik <Tatsiana_Novik@epam.com>
2021-03-13 20:56:27 +0300

    feat: add cards pictres and functionality

--- src/client/components/Home.tsx
+++ src/client/components/Home.tsx
@@ -1,46 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useCountry } from '../context/country.context';
+import { useCountry } from './../context/country.context';
+import i18next from 'i18next';
 
-const countruList = [
-  {
-    img: 'IMG',
-    name: 'Minsk',
-  },
-  {
-    img: 'IMG',
-    name: 'Pinsk',
-  },
-  {
-    img: 'IMG',
-    name: 'Mogilev',
-  },
-  {
-    img: 'IMG',
-    name: 'Gomel',
-  },
-  {
-    img: 'IMG',
-    name: 'Brest',
-  },
-];
-
+const baseUrl = 'http://127.0.0.1:3333/assets/county';
+const maxPicturesNumber = 5;
+interface IHoverCountry {
+  img: string;
+  countryName: string;
+  countryCapital: string;
+}
+function getRandomImgNumber() {
+  return Math.floor(Math.random() * Math.floor(maxPicturesNumber)) + 1;
+}
 export const Home: React.FC = () => {
   const { countries } = useCountry();
+  const [hoverCountry, setHoverCountry] = useState<IHoverCountry>();
+  function hoverHandling(event: React.MouseEvent<HTMLElement>) {
+    console.log('&&&&&&', event.currentTarget.querySelector('img').currentSrc);
+    console.log('&&&&&&', event.currentTarget.querySelectorAll('span'));
+    const countryData = event.currentTarget.querySelectorAll('span');
+    setHoverCountry({
+      img: event.currentTarget.querySelector('img').currentSrc,
+      countryName: countryData[0].getAttribute('data-countryName'),
+      countryCapital: countryData[1].getAttribute('data-countryCapital'),
+    });
+  }
+
   return (
     <>
-      <img src={`http://127.0.0.1:3333/assets/by.jpeg`} alt="" />
-      <ul>
-        {countruList.map((el, i) => {
+      {/*<img src={`http://127.0.0.1:3333/assets/by.jpeg`} alt="" />*/}
+      <div className="country-list">
+        {countries.map((el: any, i) => {
           return (
-            <li key={i}>
-              <Link to={`card/${el.name}`}>card</Link>
-            </li>
+            <div
+              className="country-item"
+              data-countryCode={el.alpha2}
+              onMouseOver={(event: React.MouseEvent<HTMLElement>) => hoverHandling(event)}
+              key={i}
+            >
+              <Link to={`/card/${el.alpha2}`}>
+                <div className="country-description">
+                  <span data-countryName={el[`name_${i18next.language}`]}>{el[`name_${i18next.language}`]}</span>
+                  <span data-countryCapital={el[`capital_${i18next.language}`]}>{el[`capital_${i18next.language}`]}</span>
+                </div>
+                <img src={`${baseUrl}/${el.alpha2}/${getRandomImgNumber()}.jpg`} alt={el.name_en} />
+              </Link>
+            </div>
           );
         })}
-        <h1>{JSON.stringify(countries)}</h1>
-      </ul>
+        {hoverCountry && (
+          <div className="hover-picture">
+            <div className="country-description">
+              <span>{hoverCountry.countryName}</span>
+              <span>{hoverCountry.countryCapital}</span>
+            </div>
+            <img src={hoverCountry.img} alt={hoverCountry.countryName} />
+          </div>
+        )}
+      </div>
     </>
   );
-  //return <Link to="card/21">card</Link>;
 };
