363339bb035901f96aa5e4e430e3bcbb0a65cafc
Tav25 <turbin28@gmail.com>
2021-03-16 10:14:45 +0300

    feat: add map

--- src/client/components/MapOnSite/MapCountry.tsx
+++ src/client/components/MapOnSite/MapCountry.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import { TileLayer } from 'react-leaflet';
 import { Marker, MapContainer } from 'react-leaflet';
-// import { StyledMapContainer } from "./App.styled";
 import './MapCountry.css';
 
 export default function MapCountry() {
-  // const { centre_lat, centre_lon } = geo;
-  // const [selectedArea, setSelectedArea] = useState({});
+
+  
   return (
-    <div className="mapContainer">
-      <MapContainer center={[10, 0]} zoom={12} scrollWheelZoom={true}>
-        {/* <Selector setSelectedArea={setSelectedArea} /> */}
+    <div className="mainMap">
+      <link rel="stylesheet" href="https://unpkg.com/leaflet@1.2.0/dist/leaflet.css" />
+      <script src="https://unpkg.com/leaflet@1.2.0/dist/leaflet.js"></script>
+      <MapContainer className="mapContainer" center={[10, 0]} zoom={12} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {/* <Area {...selectedArea} /> */}
-        <Marker position={[10, 0]}></Marker>
+        <Marker position={[10, 0]} icon="https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png"></Marker>
       </MapContainer>
     </div>
   );
